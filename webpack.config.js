e6c4ac2b881ac91e4e587823c0bf5fce8b43cff9
Crach13 <sa.mrkv@gmail.com>
2020-01-26 20:47:55 +0300

    init Project configure dev server

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract(
+                    {
+                        fallback: 'style-loader',
+                        use: ['css-loader']
+                    })
+            }
+        ]
+    }
+};
